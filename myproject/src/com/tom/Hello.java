874c7c2c5092e40575b88f595baf82a27e803065
user <user@es302n34>
2018-09-25 11:51:15 +0800

    initial

---
+++ myproject/src/com/tom/Hello.java
@@ -0,0 +1,16 @@
+package com.tom;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		float weight = (float)52.5;
+		weight = 52.5f;
+		double weight1 = 52.5;
+		short num = 8;
+		weight = num;
+		System.out.println(weight);
+
+	}
+
+}
